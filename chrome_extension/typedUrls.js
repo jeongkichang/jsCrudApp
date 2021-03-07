184da89644aba443505a012328441bf769e7f4da
Setbye <jeong.ki.chang@gmail.com>
2021-03-07 17:25:12 +0900

    add buildTypeUrlList function

--- chrome_extension/typedUrls.js
+++ chrome_extension/typedUrls.js
@@ -4,7 +4,7 @@ function onAnchorClick(event){
         url: event.srcElement.href
     });
     return false;
-}
+};
 
 function buildPopupDom(divName, data){
     var popDiv = document.getElementById(divName);
@@ -23,4 +23,70 @@ function buildPopupDom(divName, data){
         li.appendChild(a);
         ul.appendChild(li);
     }
-}
+};
+
+function buildTypeUrlList(divName){
+
+    var ms = 1000 * 60 * 60 * 24 * 7;
+    var oneWeekAgo = (new Date).getTime() - ms;
+
+    var numRequestsOutstanding = 0;
+
+    chrome.history.search({
+        startTime: oneWeekAgo,
+        text: ''
+    }, function(historyItems){
+        for(var i = 0; i < historyItems.length; ++i){
+            var url = historyItems[i].url;
+
+            var processVisitsWithUrl = function(url){
+                return function(visitItems){
+                    // url 중에서 유저가 직접 입력해서 들어간 url을 찾아서 세주는 함수
+                    processVisits(url, visitItems);
+                }
+            }
+
+            chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
+            numRequestsOutstanding++;
+        }
+
+        if(!numRequestsOutstanding){
+
+        }
+    });
+
+    var urlTocount = {};
+    var processVisits = function(url, visitItems){
+        for(var i = 0, ie = visitItems.length; i < ie; ++i){
+            if(visitItems[i].transition != 'typed'){
+                continue;
+            }
+            if(!urlToCount[url]){
+                urlTocount[url] = 0;
+            }
+
+            urlToCount[url]++;
+        }
+
+        if(!--numRequestsOutstanding){
+            onAllVisitsProceed();
+        }
+    };
+
+    var onAllVisitsProceed = function(){
+        urlArray = [];
+        for(var url in urlToCount){
+            urlArray.push(url);
+        }
+
+        urlArray.sort(function(a, b){
+            return urlToCount[b] - urlToCount[a];
+        });
+
+        buildPopupDom(divName, urlArray.slice(0, 10));
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+    buildTypedUrlList('typedUrl_div');
+});
